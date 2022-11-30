aded20fc17bf98b63a452e8b258ffe6cd7eaf9b2
momchil00 <haralampievm@gmail.com>
2022-11-30 21:37:35 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import {Routes,Route} from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import CreateGame from './components/CreateGame';
+import Catalog from './components/Catalog';
 function App() {
   return (
     <div id="box">
@@ -9,7 +13,11 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route path="/" element ={<Home/>}/>
-          
+          <Route path="/login" element ={<Login/>}/>
+          <Route path="/register" element ={<Register/>}/>
+          <Route path="/create" element ={<CreateGame/>}/>
+          <Route path="/games" element ={<Catalog/>}/>
+
         </Routes>
 
       </main>
