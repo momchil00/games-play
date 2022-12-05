fcdc6c0d086348f84f2fa2d2252160fe9a7ecfbe
momchil00 <haralampievm@gmail.com>
2022-12-05 19:47:09 +0200

    moving auth functions in context

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import { AuthContext } from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 import Logout from './components/Logout';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -11,25 +11,13 @@ import Catalog from './components/Catalog';
 import { useEffect, useState } from 'react';
 import * as gameServices from '../src/services/gameServices';
 import GameDetails from './components/GameDetails';
-import useLocalStorage from './hooks/useLocalStorage';
 import { GameContext } from './context/GameContext';
 import EditGame from './components/EditGame';
 
 function App() {
   const [games, setGames] = useState([]);
 
-  const [auth, setAuth] = useLocalStorage('auth', {});
-
   const navigate = useNavigate();
-
-
-  const userLogin = (authData) => {
-    setAuth(authData);
-  }
-
-  const userLogout = () => {
-    setAuth({});
-  };
 
   const addCommnet = (gameId, comment) => {
     setGames(state => {
@@ -52,11 +40,9 @@ function App() {
     navigate('/catalog')
   };
 
-  console.log(games, 'that is');
-
 
   const gameEdit = (gameId, gameData) => {
-    setGames(state => state.map(x => x._id === gameId ? gameData: x));
+    setGames(state => state.map(x => x._id === gameId ? gameData : x));
   }
 
   useEffect(() => {
@@ -69,18 +55,18 @@ function App() {
 
 
   return (
-    <AuthContext.Provider value={{ user: auth, userLogin, userLogout }}>
+    <AuthProvider>
       <div id="box">
         <Header />
-        <GameContext.Provider value ={{games, gameAdd,gameEdit}}>
+        <GameContext.Provider value={{ games, gameAdd, gameEdit }}>
           <main id="main-content">
             <Routes>
               <Route path="/" element={<Home games={games} />} />
               <Route path="/logout" element={<Logout />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-              <Route path="/create" element={<CreateGame/>} />
-              <Route path="/games/:gameId/edit" element={<EditGame/>} />
+              <Route path="/create" element={<CreateGame />} />
+              <Route path="/games/:gameId/edit" element={<EditGame />} />
               <Route path="/catalog" element={<Catalog games={games} />} />
               <Route path="/catalog/:gameId" element={<GameDetails games={games} addCommnet={addCommnet} />} />
 
@@ -91,7 +77,7 @@ function App() {
 
 
       </div>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,34 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import useLocalStorage from '../hooks/useLocalStorage';
 
 export const AuthContext = createContext();
+
+
+export const AuthProvider = ({
+    children,
+}) => {
+    const [auth, setAuth] = useLocalStorage('auth', {});
+
+    const userLogin = (authData) => {
+        setAuth(authData);// setvame ssesion data
+      }
+    
+      const userLogout = () => {
+        setAuth({});//mahame ssesion data
+      };
+
+    return (
+        <AuthContext.Provider value={{ user: auth, userLogin, userLogout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+
+//export const useAuthContext = () => {
+//    const context = useContext(AuthContext);
+
+//    return context
+//}
+
+//CUTOM HOOK 
